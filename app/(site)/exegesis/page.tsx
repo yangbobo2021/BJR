0e837f02e33fe65ddb7d14ae224e2f99a4286a4c
Brendan McBryde <admin@brendanjohnroch.com>
2026-02-24 19:25:45 +1300

    exegesis portal integration

--- app/(site)/exegesis/page.tsx
+++ app/(site)/exegesis/page.tsx
@@ -3,6 +3,9 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const fetchCache = "force-no-store";
 
+import React from "react";
+import PortalExegesis from "@/app/home/modules/PortalExegesis";
+
 export default function ExegesisIndexPage() {
-  return null;
+  return <PortalExegesis title="Exegesis" followPlayer={true} />;
 }
